741463010e27eb63b3e6be5f23a8075e933d66d1
Lavish Swarnkar <lavishswarnkar@gmail.com>
2019-12-26 14:22:02 +0530

    Problem MovingZeroes solved

---
+++ README.md
@@ -0,0 +1,12 @@
+**[1. Matrix Spiral Traversal:](src/P1_MatrixSpiralTraversal.java)** Given a positive integer n, generate a square matrix filled with elements from 1 to n^2 in spiral order.
+EXAMPLE:\
+Input: 3\
+Output:\
+[\
+ [1, 2, 3],\
+ [8, 9, 4],\
+ [7, 6. 5]\
+]\
+\
+**[2. MoveZeroes :](src/P2_MovingZeroes.java)** Move all zeroes to end of array
+. Given an array of random numbers, Push all the zero’s of a given array to the end of the array. For example, if the given arrays is {1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0}, it should be changed to {1, 9, 8, 4, 2, 7, 6, 0, 0, 0, 0}. The order of all other elements should be same. Expected time complexity is O(n) and extra space is O(1)
---
+++ src/P2_MovingZeroes.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+
+public class P2_MovingZeroes {
+
+    public static void main(String args[]){
+        System.out.println(Arrays.toString(moveZeroes(new int[]{0, 1, 0, 0, 3, 4, 0})));
+        System.out.println(Arrays.toString(moveZeroes(new int[]{10, 1, 3, 4})));
+    }
+
+    private static int[] moveZeroes(int[] arr){
+        int noOfZeroes = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == 0){
+                noOfZeroes++;
+            } else {
+                arr[i-noOfZeroes] = arr[i];
+            }
+        }
+        for (int i = arr.length - noOfZeroes; i < arr.length; i++)
+            arr[i] = 0;
+        return arr;
+    }
+
+}
