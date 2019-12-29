b89fd5000fca3644d06a7b661f9ee5d1295e9acc
Lavish Swarnkar <lavishswarnkar@gmail.com>
2019-12-29 15:06:02 +0530

    Solved Valid Parenthesis

---
+++ src/LeetCode/P4_ValidParenthesis.java
@@ -0,0 +1,30 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class P4_ValidParenthesis {
+
+    public static void main(String args[]){
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("([)]"));
+
+        //System.out.println(Arrays.toString(new int[]{'(', ')', '[', ']', '{', '}'}));
+    }
+
+    private static boolean isValid(String s){
+        char[] cArr = s.toCharArray();
+        Stack<Character> stack = new Stack<>();
+        for(char c : cArr){
+            if(c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                char c2 = stack.pop();
+                if(Math.abs(c2 - c) > 2)
+                    return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+}
--- src/LeetCode/README.md
+++ src/LeetCode/README.md
@@ -40,3 +40,31 @@ Example 2:
 Input: [-2,0,-1]
 Output: 0
 Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+[**4. Valid Parentheses**](P4_ValidParenthesis.java)
+https://leetcode.com/problems/valid-parentheses/
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Note that an empty string is also considered valid.
+
+Example 1:
+
+Input: "()"
+Output: true
+Example 2:
+
+Input: "()[]{}"
+Output: true
+Example 3:
+
+Input: "(]"
+Output: false
+Example 4:
+
+Input: "([)]"
+Output: false
