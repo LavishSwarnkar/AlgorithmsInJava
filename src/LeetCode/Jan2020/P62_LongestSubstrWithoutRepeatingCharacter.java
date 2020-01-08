5cdb530b7898b98f29d180b1c4c1f0f115f1c8ea
Lavish Swarnkar <lavishswarnkar@gmail.com>
2020-01-08 12:42:34 +0530

    Updated Algorithm

--- src/LeetCode/Jan2020/P62_LongestSubstrWithoutRepeatingCharacter.java
+++ src/LeetCode/Jan2020/P62_LongestSubstrWithoutRepeatingCharacter.java
@@ -3,23 +3,33 @@ package LeetCode.Jan2020;
 public class P62_LongestSubstrWithoutRepeatingCharacter {
 
     public static void main(String args[]){
-        System.out.println(lengthOfLongestSubstring("abcdabcefdvbnm"));
+        System.out.println(lengthOfLongestSubstring("abbcde"));
     }
 
     public static int lengthOfLongestSubstring(String s) {
         int maxLength = 0;
         char[] chars = s.toCharArray();
-        int repeatIndex = 0;
+        int repeatIndex = 0, start = 0, end = 0;
         for(int i=0;i<chars.length;i++){
             for(int j=repeatIndex;j<i;j++){
                 if(chars[i]==chars[j]){
-                    maxLength=Math.max(maxLength,i-repeatIndex);
+                    if(i-repeatIndex > maxLength){
+                        maxLength=i-repeatIndex;
+                        start = j+1;
+                        end = i;
+                    }
                     repeatIndex=j+1;
                     break;
                 }
             }
         }
-        return Math.max(chars.length - repeatIndex, maxLength);
+        if(chars.length - repeatIndex > maxLength){
+            maxLength = chars.length - repeatIndex;
+            start = repeatIndex;
+            end = chars.length-1;
+        }
+        System.out.println(s.substring(start, end+1));
+        return maxLength;
     }
 
 }
